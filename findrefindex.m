f60cc6a2ad7e7c47d9f7c17d27ffb9a480bbcb8d
karpob <karpob@b34e21b8-d3ac-11de-9f39-755c91dee682>
2007-12-07 15:23:01 +0000

    updated comments in findrefindex.m

--- findrefindex.m
+++ findrefindex.m
@@ -12,6 +12,7 @@ function refindex=findrefindex(T,P_H2,P_He,P_CH4,P_H2O,reference_select)
 %                             ->P_H2: Partial pressure of Hydrogen (H2) in bars
 %                             ->P_He: Partial pressure of Helium (He) in bars
 %                             ->P_CH4: Partial pressure of Methane (CH4) in bars 
+%                             -P_H2O; Partial pressure of Water vapor (H2O) in bars
 %                             ->reference_select: Select reference source
 %                                               0=No refraction ie. n=1.0D0
 %                                               1=Original DeBoer/Hoffman He/H2
@@ -27,24 +28,24 @@ function refindex=findrefindex(T,P_H2,P_He,P_CH4,P_H2O,reference_select)
 %JPH P must be in bars, T in kelvin
 
 if (reference_select==0)
-	Nr.H2=0.0.*P_H2.*(293./T);
+	Nr.H2=0.0.*P_H2.*(293./T); Should give a n=1.0 (N=0)
         Nr.He=0.0.*P_He.*(293./T);
 end
 
 if (reference_select==1)
 	Nr.H2=124.4.*P_H2.*(293./T); % Note Hoffman truncated/rounded to the nearest tenth
-	Nr.He=35.83.*P_He.*(293./T);
+	Nr.He=35.83.*P_He.*(293./T); % Both Equations from DeBoer's thesis
 end
 
 if (reference_select==2)
-	Nr.H2=136.*(P_H2./1.01325).*(273./T);
-	Nr.He=35.*(P_He./1.01325).*(273./T);
+	Nr.H2=136.*(P_H2./1.01325).*(273./T); % From Essen
+	Nr.He=35.*(P_He./1.01325).*(273./T); % From Essen
 end
 if (reference_select==3)
-        Nr.H2=136.*(P_H2./1.01325).*(273./T);
-        Nr.He=35.*(P_He./1.01325).*(273./T);
-        Nr.CH4=440.*(P_CH4./1.01325).*(273./T);
-        Nr.H2O=3.73e5.*(P_H2O.*1000).*(1./(T.*T));
+        Nr.H2=136.*(P_H2./1.01325).*(273./T); % From Essen
+        Nr.He=35.*(P_He./1.01325).*(273./T); % From Essen
+        Nr.CH4=440.*(P_CH4./1.01325).*(273./T); % From Spilker's Thesis 1990
+        Nr.H2O=3.73e5.*(P_H2O.*1000).*(1./(T.*T)); % From Thayer, 1974
 %      Add some other cool/hot gases at some point
 end
 if(reference_select<3)
