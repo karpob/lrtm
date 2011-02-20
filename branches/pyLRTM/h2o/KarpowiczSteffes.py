d77a46ef6cdda9b57191d528c611a4644e99d82d
karpob <karpob@b34e21b8-d3ac-11de-9f39-755c91dee682>
2011-02-20 15:45:04 +0000

    added h2o models to python branch

---
+++ branches/pyLRTM/h2o/KarpowiczSteffes.py
@@ -0,0 +1,81 @@
+def KarpowiczSteffes(f_in,density_h2o,density_h2,density_he,T):
+	"""
+	f_in in GHz
+	density_X mass density of h2o/h2/he
+	T in Kelvin
+	"""
+
+# This function also requires the vvwlineshape function originally written by Jim Hoffman (with removal of df factor).
+# Added shift terms from Rosenkranz.
+        from numpy import asarray,ones,power,exp,shape
+        from  vvwlineshape_modified import vvwlineshape_modified
+        mbars_to_bars=0.001
+        inv_km_to_dB=4.342945
+        convert_to_km=1e-4
+        To=300.0
+        Theta=300.0/T
+        n=len(f_in)  #returns the number of columns in f
+        nones=ones([1,n])#Vector to get 2d arrays for multiplication
+        
+        NA=6.0221415e23#molecules/mol
+        M_amu=float(8.314472/0.46151805)#g/mol
+        isotope_partition=0.997317
+        M_amu_he=4.0026
+        M_amu_h2=2.01594
+        f=f_in*nones
+        P_h2o=(1.0/M_amu)*density_h2o*8.314472e-5*T
+        P_He=(1.0/M_amu_he)*density_he*8.314310e-5*T
+        P_H2=(1.0/M_amu_h2)*density_h2*8.314472e-5*T
+        
+        density_h2o=isotope_partition*(density_h2o/M_amu)*NA*(1.0/1e6) #need in molecules/cc
+        
+        
+        # Center Frequencies in GHZ
+        f_o=asarray([[22.2351, 183.3101, 321.2256, 325.1529, 380.1974, 439.1508, 443.0183, 448.0011, 470.8890, 474.6891, 488.4911, 556.9360, 620.7008, 752.0332, 916.1712]])
+
+        # Line intensities
+        I_o=asarray([[.1314E-13, .2279E-11, .8058E-13, .2701E-11, .2444E-10,.2185E-11, .4637E-12, .2568E-10, .8392E-12, .3272E-11, .6676E-12, .1535E-08, .1711E-10, .1014E-08, .4238E-10]])
+        # Temperature coefficients
+        E_o=asarray([[2.144, .668, 6.179, 1.541, 1.048, 3.595, 5.048, 1.405,3.597, 2.379, 2.852, .159, 2.391, .396, 1.441]])
+
+
+        #self broadening parameters converted to bars
+        w_s=1.0/mbars_to_bars*asarray([[.01349, .01466, .01057,.01381,.01454,  .009715,.00788, .01275, .00983, .01095,.01313, .01405, .011836,.01253, .01275]])
+        x_s=asarray([[0.61,.85, .54, .74, .89,.62, .50,  .67, .65, .64, .72,1.0, .68, .84,.78]])
+        
+        #shift terms
+        SR=asarray([[ 0, 0, 0, 0, 0, 0.0,0.0,0.0,0.0,0.0,0.0, 0, 0.0,0.0,0.0]])
+        #Measured for 183, and 380 only
+        w_H2=asarray([2.395, 2.4000, 2.395, 2.395, 2.390, 2.395, 2.395, 2.395, 2.395, 2.395,2.395,2.395,2.395,2.395,2.395])
+        w_He=asarray([0.67, 0.71, 0.67,0.67, 0.63, 0.67, 0.67, 0.67, 0.67,0.67,0.67, 0.67, 0.67,0.67, 0.67])
+
+        x_H2=asarray([0.900, 0.950, 0.900, 0.900, 0.850, 0.900, 0.900, 0.900, 0.900, 0.900,0.900,0.900,0.900,0.900,0.900])
+        x_He=asarray([0.515, 0.490, 0.515, 0.490, 0.540, 0.515, 0.515,0.515, 0.515,0.515,0.515,0.515,0.515,0.515,0.515])
+        
+        expo=E_o*(1-Theta)
+        S=I_o*power(Theta,2.5)*exp(expo)
+
+        alpha_noshape_mat=S.T*nones
+
+        #df aka gamma delta-nu change aka pressure broadening term
+        df=w_s*P_h2o*power(Theta,x_s)+w_H2*P_H2*power(Theta,x_H2)+w_He*P_He*power(Theta,x_He)#w_f*P_f*power(Theta,x_f)#+SR*w_f*P_f*power(Theta,x_f)
+
+        # calculate van-vleck sum it up...
+        F = vvwlineshape_modified(f,f_o.T,df.T,SR.T)
+        vvw_alpha_matrix=alpha_noshape_mat*F
+        line_contribution=inv_km_to_dB*convert_to_km*density_h2o*vvw_alpha_matrix.sum(axis=0)
+
+        #Continuum Terms Foreign, and self 
+        Cf_He=((1.0/mbars_to_bars)**2)*1.03562010226e-10#  (dB/km)/((GHz x kPa)^2)->db/km((GHz bars)^2)  #eqn 10 Rosenkranz,1998
+        Cf_H2=((1.0/mbars_to_bars)**2) *5.07722009423e-11  #
+        Cs1=  4.36510480961e-07*power(Theta,13.3619799812)#   #equation 5 (correction applied from ^+6,^-6) Rosenkranz,1998
+        Cs2=  2.10003048186e-26 *((1.0/mbars_to_bars)*P_h2o)**(6.76418487001)*power(Theta,0.0435525417274)  
+        P_f=P_He+P_H2
+        Foreign_Continuum_He=Cf_He*P_He*P_h2o*power(f,2)*power(Theta,3) #foreign continuum term from Eqn 6, Rosenkranz
+        Foreign_Continuum_H2=Cf_H2*P_H2*P_h2o*power(f,2)*power(Theta,3)
+        Foreign_Continuum=Foreign_Continuum_He+Foreign_Continuum_H2
+        
+        Self_Continuum=Cs1*(P_h2o*(1.0/mbars_to_bars))**(2.0)*power(f,2.0)+Cs2*power(f,2.0)#*power(Theta,3) #self continuum term from Eqn 6, Rosenkranz
+          
+        alpha_h2o=line_contribution+inv_km_to_dB*Foreign_Continuum+inv_km_to_dB*Self_Continuum
+        return alpha_h2o
---
+++ branches/pyLRTM/h2o/vvwlineshape_modified.py
@@ -0,0 +1,63 @@
+def vvwlineshape_modified(f,fo,df,shift):
+        from numpy import ones,power,pi,abs
+        # This function determines the Van,Velck,Wiesskopf (VVW) lineshape over
+        # a range of freqencies which are user defined as is the frequency step
+        # the resulting vector is passed to this subroutine as f.
+        # fo is a vector containing the center frequencies (GHz) of the spectral 
+        # lines of the given molecule (ie PH3) as supplied by the "Submillimeter,
+        # Millimeter, and Microwave Spectral Catalog" aka Poynter-Pickett Line Catalog
+        # but in GHz, not in MHz as provided by the catalog.
+        # df is elsewhere called d_nu in the main program and is the line half-width at
+        # half-maximum or simply linewidth
+        # 
+        # Bryan Karpowicz: Removed df term so fvvw is actually fvvw, and not
+        # df*fvvw. Also added cutoff at 750 GHz for consistency with Rosenkranz.
+        #
+        #
+        # NOTE: All frequencies are or have been converted to inverse cm!!!
+
+        # fvvw(f,fo,df)=(1/pi)*(f/fo)^2*(    df          +         df	   )
+        #                              --------------     --------------      -       base
+        #                              (fo-f-shift)^2 +df^2     (fo+f+shift)^2 +df^2
+
+        # F =                A      *(     B          +         C       )
+
+        n=len(f)
+        m=len(fo)
+        # f(1) f(2) f(3) f(4) ....f(n) n times where n is the number of frequency steps
+        # f(1) f(2) f(3) f(4)				in the observation range                            
+        # ...
+        # m times where m is the number of spectral lines
+
+        nones=ones([1,n])
+        mones=ones([m,1])
+
+        f_matrix=mones*f
+        fo_matrix=fo*nones
+        df_matrix=df*nones      
+        shift_mat=shift*nones
+        df_minus=f_matrix-fo_matrix-shift_mat
+        df_plus=f_matrix+fo_matrix+shift_mat
+        
+        cut_off=750
+
+        base=df/(562500.0+power(df,2))*nones
+        
+        A=(1.0/pi)*power((f_matrix/fo_matrix),2);			# f(1)*fo(1)  f(2)*fo(1) f(3)*fo(1)
+                                                        # f(1)*fo(2)  f(2)*fo(2) f(3)*fo(2)
+        B=df_matrix/(power((df_minus),2)+power(df_matrix,2))
+        C=df_matrix/(power((df_plus),2)+power(df_matrix,2))
+
+        filter1=abs(f_matrix-fo_matrix-shift_mat)<=cut_off;
+        filter2=abs(f_matrix+fo_matrix+shift_mat)<=cut_off;
+        
+        
+
+
+        F=A*(filter1*(B-base)+filter2*(C-base))#.*df_matrix;						% m x n matrix
+        
+        return F
+        # Note: the extra df factor comes in because of the expression for alpha has
+        # an additional df factor which cancels with the alpha_max, but is added here
+        # for computational reasons.  result is actually df*Fvvw
+
