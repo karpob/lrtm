03795b6e5f8374d9c089c0be50e9c0c6ce19156b
karpob <karpob@b34e21b8-d3ac-11de-9f39-755c91dee682>
2011-02-20 19:12:56 +0000

    added h2slin to tags

--- TCM_mex/python_compressibility/helmholtz_functions/ideal_helmholtz_from_coef_dtau.py
+++
@@ -1,6 +0,0 @@
-def ideal_helmholtz_from_coef_dtau(delta,tau,ideal_n,ideal_gamma):
-    from math import pow,exp
-    t1 = 0.0
-    for i in range(3,len(ideal_gamma)):
-        t1 += ideal_n[i]*ideal_gamma[i]*(pow(1.0 - exp(-ideal_gamma[i]*tau),-1.0) - 1.0)
-    return ideal_n[1] + ideal_n[2]/tau + t1
--- plots/rdcheck.m
+++
@@ -1,7 +0,0 @@
-X=rd(:,1);
-Y=rd(:,2);
-Z=rd(:,3);
-
-[TH,PHI,R] = CART2SPH(X,Y,Z);
-
-polar(PHI,R)
